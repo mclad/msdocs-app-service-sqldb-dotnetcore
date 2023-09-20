439a2ab066d19453a34e808c8702d7780dba9c2b
mclad <145580899+mclad@users.noreply.github.com>
2023-09-20 14:16:11 -0300

    update

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")));
 builder.Services.AddDistributedMemoryCache();
 
 // Add services to the container.
